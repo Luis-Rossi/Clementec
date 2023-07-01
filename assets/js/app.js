38928286d1d1cdbe54b61c5f3813abaa0ade1bcc
Luis Rossi <luisrossi89@gmail.com>
2023-06-30 23:51:47 -0300

    active navbar links

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
 // SCROLL SECTIONS
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
 
 window.onscroll = () => {
+    // text
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height) {
+            // active navbar links
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        }
+    });
+
     // when scroll over 100px, header receive sticky CSS properties
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
