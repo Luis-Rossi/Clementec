90223cb0a2c008814c7a1bf20c02abecb6cd9c8e
Luis Rossi <luisrossi89@gmail.com>
2023-07-01 00:49:44 -0300

    working toggle buttom

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,5 +21,19 @@ window.onscroll = () => {
 
     // when scroll over 100px, header receive sticky CSS properties
     let header = document.querySelector('header');
+
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon when click navbar links (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+}
+
+// TOGGLE ICON NAVBAR
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
 }
